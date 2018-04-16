c86063f2be58c8a95a756e4e132f6e9b2d36c62d
djwojtas <djwojtas-96@o2.pl>
2018-04-17 01:02:06 +0200

    cors testing

--- src/main/java/pl/edu/agh/geoxplore/security/CorsFilter.java
+++ src/main/java/pl/edu/agh/geoxplore/security/CorsFilter.java
@@ -15,13 +15,17 @@ public class CorsFilter implements Filter {
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) servletResponse;
-        HttpServletRequest request= (HttpServletRequest) servletRequest;
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
 
-        response.setHeader("Access-Control-Allow-Origin", "*");
-        response.setHeader("Access-Control-Allow-Methods", "GET,POST,DELETE,PUT,OPTIONS");
-        response.setHeader("Access-Control-Allow-Headers", "*");
-        response.setHeader("Access-Control-Allow-Credentials", "true");
-        response.setHeader("Access-Control-Max-Age", "180");
+        //System.out.println(request.getRemoteHost()); //commented for easier webapp tests
+        //if(request.getRemoteHost().equals("http://localhost:8080")) {
+            response.setHeader("Access-Control-Allow-Origin", "*");
+            response.setHeader("Access-Control-Allow-Methods", "GET,POST,DELETE,PUT,OPTIONS");
+            response.setHeader("Access-Control-Allow-Headers", "*");
+            response.setHeader("Access-Control-Allow-Credentials", "true");
+            response.setHeader("Access-Control-Max-Age", "180");
+        //}
+
         filterChain.doFilter(servletRequest, servletResponse);
     }
 
